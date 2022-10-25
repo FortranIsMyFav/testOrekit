4d3ea13c38db8e6b2265acc4cc2051d3748f2092
shane moran <shane.c.moran@gmail.com>
2022-10-24 19:15:59 -0600

    add simple test

---
+++ src/test/java/com/scmtf/testorekit/MainTest.java
@@ -0,0 +1,12 @@
+package com.scmtf.testorekit;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
